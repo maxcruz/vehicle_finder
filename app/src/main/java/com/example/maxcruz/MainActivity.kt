8af4c083ca322d1fb5426d6f40402a10b02676a8
Max Cruz <max.raul@gmail.com>
2018-11-10 17:30:59 -0500

    Base presentation layer

---
+++ app/src/main/java/com/example/maxcruz/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.maxcruz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.maxcruz.vehiclefinder.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
