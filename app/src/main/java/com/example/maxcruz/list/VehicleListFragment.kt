4190ac91449db77f45092d36bc959fbdf149851c
Max Cruz <max.raul@gmail.com>
2018-11-11 21:36:56 -0500

    Pick a less generic name for list fragment

---
+++ app/src/main/java/com/example/maxcruz/list/VehicleListFragment.kt
@@ -0,0 +1,58 @@
+package com.example.maxcruz.list
+
+
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.ListFragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.maxcruz.VehicleFinderApplication
+import com.example.maxcruz.list.adapters.VehicleAdapter
+import com.example.maxcruz.vehiclefinder.R
+import com.example.maxcruz.vehiclefinder.databinding.FragmentListBinding
+import kotlinx.android.synthetic.main.fragment_list.*
+import javax.inject.Inject
+
+
+/**
+ * Display a list of vehicles to select
+ */
+class ListFragment : Fragment() {
+
+    private lateinit var binding: FragmentListBinding
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (context.applicationContext as VehicleFinderApplication).getListComponent().inject(this as ListFragment)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
+        binding.vm = ViewModelProviders.of(this, viewModelFactory)[ListViewModel::class.java]
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupList()
+    }
+
+    private fun setupList() {
+        val adapter = VehicleAdapter {
+
+        }
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+    }
+
+
+}
---
+++ app/src/main/java/com/example/maxcruz/list/VehicleListViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.maxcruz.list
+
+import android.arch.lifecycle.ViewModel
+import com.example.maxcruz.domain.interactors.GetPoints
+import com.example.maxcruz.domain.models.Point
+import javax.inject.Inject
+
+class ListViewModel @Inject constructor(private val getPoints: GetPoints): ViewModel() {
+
+    var pointList = listOf<Point>()
+
+}
